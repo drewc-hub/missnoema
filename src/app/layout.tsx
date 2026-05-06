9e9e76a498a7918dd94e5e97a84eafd6cc73da44
andrewc <andrewc@localhost>
2026-05-06 16:38:51 -0500

    attempt to fix magic link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,26 +2,26 @@ import "./globals.css";
 import type { Metadata } from "next";
 import React from "react";
 import { Footer } from "@/components/Footer";
-import { TopNav } from "@/components/nav";
+import { safenav } from "@/components/safenav";
 export const metadata: Metadata = {
-  title: "NOMEA",
-  description: "Companion library + custom companions",
+    title: "NOMEA",
+    description: "Companion library + custom companions",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-     <body>
-      <TopNav />
-      <div className="mx-auto w-full max-w-6xl px-4">
-        <div className="py-8 rounded-lg-:em">{children}</div>
-        <Footer className="rounded-[24px]" />
-      </div>
-     </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <safenav />
+                <div className="mx-auto w-full max-w-6xl px-4">
+                    <div className="py-8 rounded-lg-:em">{children}</div>
+                    <Footer className="rounded-[24px]" />
+                </div>
+            </body>
+        </html>
+    );
 }
