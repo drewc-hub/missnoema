52ebabdbe0aa84a0f8f5ab429b698e645b518da0
andrewc <andrewc@localhost>
2026-05-09 04:50:43 -0500

    added viewport metadata

--- src/app/adult/layout.tsx
+++ src/app/adult/layout.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { AdultFooter } from "@/components/AdultFooter";
 import { AdultTopNav } from "@/components/Adultnav";
+import type { Viewport } from 'next'
+
+export const viewport: Viewport = {
+    themeColor: 'black',
+    width: 'device-width',
+    initialScale: 1,
+    // Useful for mobile devices with notches
+    viewportFit: 'cover',
+}
 
 export default function AdultLayout({
     children,
